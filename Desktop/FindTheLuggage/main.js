8ccf8c0fd3357707c4d25f7c7fb6b9be9ba4fe5a
Ahmet Efeoglu <ah508830@dal.ca>
2020-03-22 19:29:17 -0300

    First version of this project is complete.

---
+++ Desktop/FindTheLuggage/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+function toggl() {
+    var results = document.getElementById("results");
+    
+
+    if (results.style.display = "none") {
+        results.style.display = "block";
+    }
+
+    else {
+        results.style.display = "none";
+    }
+
+}
